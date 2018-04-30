8a929cd4b9d8431550d6a566d076ee757a054d53
GuoYankai1996 <GuoYankai1996@outlook.com>
2018-04-30 22:25:56 +0800

    111
    1111

--- src/GUI.java
+++ src/GUI.java
@@ -1,10 +1,13 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.sql.Connection;
@@ -22,10 +25,10 @@ import javax.swing.JFrame;
 import ip.IP;
 
 public class GUI {
+	public static String address;
 	public static AtomicInteger a=new AtomicInteger(0);
-	static volatile IP ip=new IP("202.168.244.106",53281);
 	public static void main(String[] args){
-		
+		LinkedList<String> list=getAddressList();
 		long time=0;
 		int temp=0;
 		JFrame f=new JFrame("control");
@@ -37,6 +40,13 @@ public class GUI {
 		b1.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
 				GetSongs.getSongs();
+				for(int i=0;i<14;i++){
+					Thread a=new GetSongsThread(GetSongs.queue,GetSongs.songqueue);
+					a.start();
+				}
+				for(int m=0;m<1;m++){
+					new SongqueueThread(GetSongs.songqueue,GetSongs.queue).start();
+				}
 			}
 		});
 		b2.addActionListener(new ActionListener(){
@@ -62,10 +72,22 @@ public class GUI {
 		f.setVisible(true);
 		time=System.currentTimeMillis();
 		while(true){
-			if(GetSongsThread.count.get()>300){
-				GetSongsThread.j=++GetSongsThread.j%15;
-				GetSongs.iplist.add(ip);
-				ip=GetSongs.iplist.poll();
+			if(GetSongsThread.count.get()>600){
+				String string;
+				SetNewIP(string=list.poll());
+				list.add(string);
+				long time1=System.currentTimeMillis();
+				while(true){
+					int a1=GetSongsThread.count.get();
+					try {
+						Thread.sleep(20000);
+					} catch (InterruptedException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					if((System.currentTimeMillis()-time1)>120000) break;
+					if(a1==GetSongsThread.count.get()) break;
+				}
 				GetSongsThread.count.set(0);
 			}
 			try {
@@ -74,7 +96,7 @@ public class GUI {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-			if((System.currentTimeMillis()-time)>600000){
+			if((System.currentTimeMillis()-time)>60000){
 				time=System.currentTimeMillis();
 				try(Connection c= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/music?characterEncoding=UTF-8","root", "admin");
 						Statement s=c.createStatement();){
@@ -89,7 +111,7 @@ public class GUI {
 					e1.printStackTrace();
 				}
 			}
-			if(GetSongs.temp&&SongqueueThread.count.get()==15){
+			if(GetSongs.temp&&SongqueueThread.count.get()==14){
 				try {
 					IOUtil.OutputSongQueue(GetSongs.songqueue);
 					IOUtil.OutputRepeat(GetSongs.repeat);
@@ -105,5 +127,40 @@ public class GUI {
 			}
 			
 		}
+	}
+	public static void SetNewIP(String address){
+		try {
+			Runtime.getRuntime().exec("cmd /c echo yes|reg add HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Class\\{4D36E972-E325-11CE-BFC1-08002BE10318}\\0012 /v NetworkAddress /t REG_SZ /d "+address);
+			Runtime.getRuntime().exec("cmd /c netsh interface set interface 本地连接 disabled");
+			Thread.sleep(3000);
+			Runtime.getRuntime().exec("cmd /c netsh interface set interface 本地连接 enabled");
+			Thread.sleep(5000);
+				Runtime.getRuntime().exec("cmd.exe /c taskkill /F /IM \"iNode*\" /T");
+				Thread.sleep(3000);
+				
+				Runtime.getRuntime().exec("cmd.exe /c start C:\\\"Program Files (x86)\"\\H3C\\\"iNode Client\"\\\"iNode Client.exe\"");
+			
+			System.out.println("切换物理地址");
+		} catch (IOException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static LinkedList<String> getAddressList(){
+		LinkedList<String> list=new LinkedList<String>();
+		File file=new File("E:\\workspace\\MusicCrawler\\address.txt");
+		try {
+			BufferedReader in=new BufferedReader(new FileReader(file));
+			String string;
+			while((string=in.readLine())!=null){
+				list.add(string);
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+		
 	}
 }
